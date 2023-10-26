ed02474b6f5813419202b4fce1035c366d4e709b
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-26 16:53:21 +0300

    Add project files.

---
+++ Ders 46 - Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_46___Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer { FirstName = "Engin" },
+                new Student { FirstName = "Derin" },
+                new Person { FirstName = "Salih" }
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    interface IPerson
+    {
+
+    }
+    class Customer:Person, IPerson //Buraya da person2 ekleyemiyoruz çünkü bir kişinin bir babası olur
+    {                     //ama interface olduğu için IPerson u ekleyebildik
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
